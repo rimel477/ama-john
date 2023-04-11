669f65dbef16464843afb5325da5865bdf95eed4
rimel477 <rimel477@gmail.com>
2023-04-11 21:37:08 +0600

    my react first app

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import {} from "react-router-dom"
+import "./App.css";
+import Header from "./components/Header/Header";
+import Shop from "./components/Shop/Shop.js";
+
+
+function App() { 
+  return (
+    <div>
+      <Header></Header>
+      <Shop></Shop>
+    </div>
+  );
+}
+
+export default App;
